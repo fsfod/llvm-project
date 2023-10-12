0f21403d3eeb7c4ae3dccbeb2c399bc9792c79b1
Tom Stellard <tstellar@redhat.com>
2023-10-12 04:42:37 +0000

    XXX: manual: Domtree Functions
    XXX: Manual PassManager
    
    XXX: Manual ELFFile
    
    XXX: Manual XCOFFObjectFile
    
    XXX: Manual DomTree template
    
    XXX: Manual: LoopInfoBase Template

--- llvm/include/llvm/Object/XCOFFObjectFile.h
+++ llvm/include/llvm/Object/XCOFFObjectFile.h
@@ -731,6 +731,11 @@ public:
   std::optional<StringRef> tryGetCPUName() const override;
 }; // XCOFFObjectFile
 
+extern template Expected<ArrayRef<ExceptionSectionEntry32>> LLVM_TEMPLATE_ABI
+XCOFFObjectFile::getExceptionEntries() const;
+extern template Expected<ArrayRef<ExceptionSectionEntry64>> LLVM_TEMPLATE_ABI
+XCOFFObjectFile::getExceptionEntries() const;
+
 typedef struct {
   uint8_t LanguageId;
   uint8_t CpuTypeId;
