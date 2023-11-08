167eb7f552321d5314bae6e6d03af9b59f7dfcfe
Tom Stellard <tstellar@redhat.com>
2023-11-08 15:58:48 +0000

    Consolidate MemoryProfile cl declaration into headers

--- llvm/lib/Analysis/ModuleSummaryAnalysis.cpp
+++ llvm/lib/Analysis/ModuleSummaryAnalysis.cpp
@@ -81,8 +81,6 @@ static cl::opt<std::string> ModuleSummaryDotFile(
     "module-summary-dot-file", cl::Hidden, cl::value_desc("filename"),
     cl::desc("File to emit dot graph of new summary into"));
 
-extern cl::opt<bool> ScalePartialSampleProfileWorkingSetSize;
-
 extern cl::opt<unsigned> MaxNumVTableAnnotations;
 
 // Walk through the operands of a given User via worklist iteration and populate
