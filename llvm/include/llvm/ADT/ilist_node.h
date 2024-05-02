9699f75836855b440a5e1c34b98a13832350ca93
fsfod <fsfod11@gmail.com>
2024-05-02 18:52:23 +0100

    Try to fix ealier ilist_node changes

--- llvm/include/llvm/ADT/ilist_node.h
+++ llvm/include/llvm/ADT/ilist_node.h
@@ -125,10 +125,14 @@ public:
   ///
   /// This requires sentinel tracking to be explicitly enabled.  Use the
   /// ilist_sentinel_tracking<true> option to get this API.
-  template<typename T = OptionsT> typename std::enable_if_t<T::is_sentinel_tracking_explicit, bool>::type isSentinel() const {
+  template<typename T = OptionsT> typename std::enable_if_t<T::enable_sentinel_tracking, bool> isSentinel() const {
    // static_assert(OptionsT::is_sentinel_tracking_explicit,
    //               "Use ilist_sentinel_tracking<true> to enable isSentinel()");
     return node_base_type::isSentinel();
+  }
+
+  template<typename T = OptionsT> typename std::enable_if_t<!T::enable_sentinel_tracking, bool> isSentinel() const {
+    return false;
   }
 };
 
