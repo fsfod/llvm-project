7ed73c122c4d8d6ca9a9c2e6f2d565b4acb66002
Thomas Fransham <tfransham@gmail.com>
2024-06-01 00:25:13 +0100

    Set MultiHazardRecognizer copy constructor and assignment as deleted
    It has non copyable members holding std::unique_ptr

--- llvm/include/llvm/CodeGen/MultiHazardRecognizer.h
+++ llvm/include/llvm/CodeGen/MultiHazardRecognizer.h
@@ -27,6 +27,8 @@ class MultiHazardRecognizer : public ScheduleHazardRecognizer {
 
 public:
   MultiHazardRecognizer() = default;
+  MultiHazardRecognizer(const MultiHazardRecognizer &) = delete;
+  MultiHazardRecognizer &operator =(const MultiHazardRecognizer &) = delete;
   void AddHazardRecognizer(std::unique_ptr<ScheduleHazardRecognizer> &&);
 
   bool atIssueLimit() const override;
