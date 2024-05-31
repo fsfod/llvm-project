a517db95d51a246326abde6ac87f7149afd14dec
Thomas Fransham <tfransham@gmail.com>
2024-05-31 05:33:18 +0100

    Add auto generated macros to the Option library

--- llvm/include/llvm/Option/OptTable.h
+++ llvm/include/llvm/Option/OptTable.h
@@ -50,7 +50,7 @@ public:
 /// be needed at runtime; the OptTable class maintains enough information to
 /// parse command lines without instantiating Options, while letting other
 /// parts of the driver still use Option instances where convenient.
-class OptTable {
+class LLVM_CLASS_ABI OptTable {
 public:
   /// Entry for a single option instance in the option data table.
   struct Info {
@@ -353,7 +353,7 @@ private:
 };
 
 /// Specialization of OptTable
-class GenericOptTable : public OptTable {
+class LLVM_CLASS_ABI GenericOptTable : public OptTable {
   SmallVector<StringLiteral> PrefixesUnionBuffer;
 
 protected:
