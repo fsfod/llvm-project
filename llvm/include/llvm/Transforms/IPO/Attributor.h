99b3d661045bc1598f70fa81c9a02a5b2f199d8f
Thomas Fransham <tfransham@gmail.com>
2024-06-11 20:34:55 +0100

    Add manual export macros for AAPotentialConstantValues andAAPotentialValues  to work around clang bug
    "Clang prohibits initialization of static members of specialized __declspec(dllexport) class templates"
    https://github.com/llvm/llvm-project/issues/54814
    The template class PotentialValuesState that both classes derive from has
    static field that they both declare a definition for as a explicit specialization in the source file.

--- llvm/include/llvm/Transforms/IPO/Attributor.h
+++ llvm/include/llvm/Transforms/IPO/Attributor.h
@@ -5132,6 +5132,8 @@ private:
   bool UndefIsContained;
 };
 
+template <typename MemberTy> unsigned PotentialValuesState<MemberTy>::MaxPotentialValues = -1;
+
 struct DenormalFPMathState : public AbstractState {
   struct DenormalState {
     DenormalMode Mode = DenormalMode::getInvalid();
@@ -5263,7 +5265,7 @@ struct AAPotentialConstantValues
   }
 
   /// Create an abstract attribute view for the position \p IRP.
-  static AAPotentialConstantValues &createForPosition(const IRPosition &IRP,
+  LLVM_ABI static AAPotentialConstantValues &createForPosition(const IRPosition &IRP,
                                                       Attributor &A);
 
   /// Return assumed constant for the associated value
@@ -5301,7 +5303,7 @@ struct AAPotentialConstantValues
   }
 
   /// Unique ID (due to the unique address)
-  static const char ID;
+  LLVM_ABI static const char ID;
 };
 
 struct AAPotentialValues
