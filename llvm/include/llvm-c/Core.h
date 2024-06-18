2fd896f8087bcb028dc8c2a79bec5697dbbec98d
Thomas Fransham <tfransham@gmail.com>
2024-06-18 04:44:56 +0100

    Update manual export for llvm-c isa functions declared by a macro

--- llvm/include/llvm-c/Core.h
+++ llvm/include/llvm-c/Core.h
@@ -1925,7 +1925,7 @@ LLVM_C_ABI LLVMBool LLVMIsPoison(LLVMValueRef Val);
  * @see llvm::dyn_cast_or_null<>
  */
 #define LLVM_DECLARE_VALUE_CAST(name) \
-  LLVM_FUNC_ABI LLVMValueRef LLVMIsA##name(LLVMValueRef Val);
+  LLVM_C_ABI LLVMValueRef LLVMIsA##name(LLVMValueRef Val);
 LLVM_FOR_EACH_VALUE_SUBCLASS(LLVM_DECLARE_VALUE_CAST)
 
 LLVM_C_ABI LLVMValueRef LLVMIsAMDNode(LLVMValueRef Val);
