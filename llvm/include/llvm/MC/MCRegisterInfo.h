0a4bc54f1884fe72a6a2008a9d2f3020e4892487
Thomas Fransham <tfransham@gmail.com>
2024-06-22 17:23:07 +0100

    Use forward_iterator_facade_base instead of iterator_facade_base  for DiffListIterator

--- llvm/include/llvm/MC/MCRegisterInfo.h
+++ llvm/include/llvm/MC/MCRegisterInfo.h
@@ -191,8 +191,8 @@ private:
   /// Iterator class that can traverse the differentially encoded values in
   /// DiffLists. Don't use this class directly, use one of the adaptors below.
   class DiffListIterator
-      : public forward_iterator_facade_base<
-            DiffListIterator, std::forward_iterator_tag, unsigned> {
+      : public iterator_facade_base<DiffListIterator, std::forward_iterator_tag,
+                                    unsigned> {
     unsigned Val = 0;
     const int16_t *List = nullptr;
 
@@ -213,7 +213,7 @@ private:
     /// Dereference the iterator to get the value at the current position.
     const unsigned &operator*() const { return Val; }
 
-    using DiffListIterator::forward_iterator_facade_base::operator++;
+    using DiffListIterator::iterator_facade_base::operator++;
     /// Pre-increment to move to the next position.
     DiffListIterator &operator++() {
       assert(isValid() && "Cannot move off the end of the list.");
