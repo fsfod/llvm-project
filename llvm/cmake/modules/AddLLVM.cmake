471081df9e5ffe930afdc1bebdeaef97963e9887
Thomas Fransham <tfransham@gmail.com>
2024-06-24 16:24:38 +0100

    Add a cmake option to force dll exporting inline class members when building with clang-cl so the dll will be compatible with MSVC

--- llvm/cmake/modules/AddLLVM.cmake
+++ llvm/cmake/modules/AddLLVM.cmake
@@ -625,7 +625,7 @@ function(llvm_add_library name)
   ## class members from being dllexport'ed to reduce compile time.
   ## This will also keep us below the 64k exported symbol limit
   ## https://blog.llvm.org/2018/11/30-faster-windows-builds-with-clang-cl_14.html
-  if(LLVM_BUILD_LLVM_DYLIB AND MSVC AND CMAKE_CXX_COMPILER_ID MATCHES Clang)
+  if(LLVM_BUILD_LLVM_DYLIB AND NOT LLVM_DYLIB_EXPORT_INLINES AND MSVC AND CMAKE_CXX_COMPILER_ID MATCHES Clang)
     target_compile_options(${name} PUBLIC /Zc:dllexportInlines-)
   endif()
 
@@ -1104,9 +1104,11 @@ macro(add_llvm_executable name)
 
   llvm_codesign(${name} ENTITLEMENTS ${ARG_ENTITLEMENTS} BUNDLE_PATH ${ARG_BUNDLE_PATH})
 
-  if (LLVM_LINK_LLVM_DYLIB AND NOT ARG_DISABLE_LLVM_LINK_LLVM_DYLIB)
-    target_compile_definitions(${name} PRIVATE -DLLVM_DLL_IMPORT)
-    if(MSVC AND CMAKE_CXX_COMPILER_ID MATCHES Clang)
+  if (LLVM_BUILD_LLVM_DYLIB OR LLVM_ENABLE_PLUGINS OR LLVM_EXPORT_SYMBOLS_FOR_PLUGINS)
+    if(ARG_DISABLE_LLVM_LINK_LLVM_DYLIB)
+      target_compile_definitions(${name} PRIVATE LLVM_BUILD_STATIC)
+    elseif(NOT LLVM_DYLIB_EXPORT_INLINES AND MSVC AND CMAKE_CXX_COMPILER_ID MATCHES Clang)
+      # This has to match how the libraries the executable is linked to are built or there be linker errors.
       target_compile_options(${name} PRIVATE /Zc:dllexportInlines-)
     endif()
   endif()
