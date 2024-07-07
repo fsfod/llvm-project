70a37c8e332a9a5fb444a07ede19b434560c65f2
Thomas Fransham <tfransham@gmail.com>
2024-07-07 22:36:03 +0100

    Add auto generated export macros to the clang Serialization library

--- clang/include/clang/Serialization/ASTBitCodes.h
+++ clang/include/clang/Serialization/ASTBitCodes.h
@@ -24,6 +24,7 @@
 #include "clang/Basic/OperatorKinds.h"
 #include "clang/Basic/SourceLocation.h"
 #include "clang/Serialization/SourceLocationEncoding.h"
+#include "clang/Support/Compiler.h"
 #include "llvm/ADT/DenseMapInfo.h"
 #include "llvm/Bitstream/BitCodes.h"
 #include <cassert>
@@ -2011,7 +2012,7 @@ struct ObjCCategoriesInfo {
 /// Different \ref DeclarationNames are mapped to different keys, but the
 /// same key can occasionally represent multiple names (for names that
 /// contain types, in particular).
-class DeclarationNameKey {
+class CLANG_ABI DeclarationNameKey {
   using NameKind = unsigned;
 
   NameKind Kind = 0;
