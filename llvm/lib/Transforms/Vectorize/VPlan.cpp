abbdbebadc19871fad9a270e97a72b6b5ca92c0d
Thomas Fransham <tfransham@gmail.com>
2024-09-08 14:50:11 +0100

    Re-add export explicit template instantiation of DomTreeBuilder::Calculate<DomTreeBase<VPBlockBase>> needed by VPlan tests

--- llvm/lib/Transforms/Vectorize/VPlan.cpp
+++ llvm/lib/Transforms/Vectorize/VPlan.cpp
@@ -24,6 +24,7 @@
 #include "VPlanPatternMatch.h"
 #include "VPlanTransforms.h"
 #include "VPlanUtils.h"
+#include "VPlanDominatorTree.h "
 #include "llvm/ADT/PostOrderIterator.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SmallVector.h"
@@ -45,6 +46,7 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Transforms/Utils/LoopVersioning.h"
+#include "llvm/Support/GenericDomTreeConstruction.h"
 #include <cassert>
 #include <string>
 
@@ -53,6 +55,10 @@ using namespace llvm::VPlanPatternMatch;
 
 namespace llvm {
 extern cl::opt<bool> EnableVPlanNativePath;
+
+ template void LLVM_EXPORT_TEMPLATE
+DomTreeBuilder::Calculate<DomTreeBase<VPBlockBase>>(
+  DomTreeBase<VPBlockBase> &DT);
 }
 
 extern cl::opt<unsigned> ForceTargetInstructionCost;
