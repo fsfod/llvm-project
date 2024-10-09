d428eea23ba6cca2d712ad1651f7a0d3108bae4a
Thomas Fransham <tfransham@gmail.com>
2024-10-09 21:32:50 +0100

    Fix warning for DbgVariableRecord from implcitit default assignment operator when a non implicit copy constructor is declared

--- llvm/include/llvm/IR/DebugProgramInstruction.h
+++ llvm/include/llvm/IR/DebugProgramInstruction.h
@@ -299,6 +299,7 @@ public:
                     DIExpression *Expression, DIAssignID *AssignID,
                     Metadata *Address, DIExpression *AddressExpression,
                     const DILocation *DI);
+  DbgVariableRecord &operator =(const DbgVariableRecord &) = default;
 
 private:
   /// Private constructor for creating new instances during parsing only. Only
