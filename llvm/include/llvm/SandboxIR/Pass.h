15bbc954c2b8fe61bbc1b71b3858c7156ee2b5d9
Thomas Fransham <tfransham@gmail.com>
2024-10-23 06:53:43 +0100

    Add missing visibility macros for SandboxIR

--- llvm/include/llvm/SandboxIR/Pass.h
+++ llvm/include/llvm/SandboxIR/Pass.h
@@ -39,7 +39,7 @@ public:
 };
 
 /// The base class of a Sandbox IR Pass.
-class Pass {
+class LLVM_ABI Pass {
 protected:
   /// The pass name. This is also used as a command-line flag and should not
   /// contain whitespaces.
